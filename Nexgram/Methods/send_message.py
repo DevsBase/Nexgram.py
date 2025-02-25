81fb129ccb49ea06ecfe692b81842f02d7cd19a0
Otazuki004 <otazuki004@gmail.com>
2025-02-25 13:03:12 +0530

    Update

--- Nexgram/Methods/send_message.py
+++ Nexgram/Methods/send_message.py
@@ -1,6 +1,6 @@
 import httpx
 
-class SendMessage:
+class sendMessage:
   async def send_message(self, chat_id, text):
     if not self.connected: raise ConnectionError("Client is not connected, you must connect the client to send message.")
     url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"

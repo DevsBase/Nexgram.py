6cbf223d585208744bdcc9bb124c87a702dc77ae
Otazuki004 <otazuki004@gmail.com>
2025-02-25 12:54:11 +0530

    Update

--- Nexgram/client.py
+++ Nexgram/client.py
@@ -1,38 +1,22 @@
 import logging
-import struct
-import time
-import os
-import hashlib
-import asyncio
-from .connection import Connection
+import httpx
+from .Methods import *
 
 log = logging.getLogger(__name__)
 
-class Client:
-  def __init__(
-    self,
-    name: str,
-    api_id: int,
-    api_hash: str,
-    bot_token: str = None,
-    session_string: str = None      
-  ):
+class Client(Methods):
+  def __init__(self, name: str, bot_token: str):
     self.name = name
-    self.api_id = api_id
-    self.api_hash = api_hash
     self.bot_token = bot_token
-    self.session_string = session_string
-    self.auth_key = None
-    self.socket = Connection("91.108.56.165", 443)
+    self.connected = False
+    self.me = None
 
-  async def connect(self):
-    await self.socket.connect()
-
-  async def req_pq(self):
-    random_bytes = os.urandom(16)
-    message_id = int(time.time() * (2**32))
-    payload = struct.pack("<Q", message_id) + random_bytes
-
-    await self.socket.send(payload)
-    response = await self.socket.receive()
-    return response
+  async def start(self):
+    url = f"https://api.telegram.org/bot{self.bot_token}/getMe"
+    async with httpx.AsyncClient() as client:
+      r = (await client.get(url)).json()
+      if r.get("ok"):
+        self.connected = True
+        self.me = r["result"]
+        log.info(f"Client connected as {self.me['first_name']} (@{self.me['username']})")
+      return r
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup( 
   name="Nexgram.py",
-  version="0.0.10",
+  version="0.0.11",
   packages=find_packages(),
   install_requires=[
     "aiofiles>=24.1.0",
-    "pycryptodome>=3.20.0",
+    "httpx>=0.26.0",
   ],
   author="Otazuki",
   author_email="otazuki004@gmail.com",
-  description="just a try to beat pyrogram :)",
+  description="just a try :)",
   long_description=open("README.md").read(),
   long_description_content_type="text/markdown",
   url="https://github.com/Otazuki004/Nexgram.py",

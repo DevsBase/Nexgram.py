47d1570b346ebc488d663673c79f39992826bd8f
Otazuki004 <otazuki004@gmail.com>
2025-02-25 11:53:43 +0530

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup( 
   name="Nexgram.py",
-  version="0.0.6",
+  version="0.0.7",
   packages=find_packages(),
   install_requires=[
     "aiofiles>=24.1.0",

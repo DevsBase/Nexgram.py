684a3c617491fa520840c131cb4073276de3c306
Otazuki004 <otazuki004@gmail.com>
2025-02-26 18:55:03 +0530

    Update

--- Nexgram/methods/send_message.py
+++ Nexgram/methods/send_message.py
@@ -4,7 +4,8 @@ class sendMessage:
   async def send_message(self, chat_id, text, parse_mode=None):
     if not self.connected: raise ConnectionError("Client is not connected, you must connect the client to send message.")
     url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
-    data = {"chat_id": chat_id, "text": text, "parse_mode": parse_mode or self.global_parse_mode}
+    parse_mode = parse_mode or self.global_parse_mode
+    data = {"chat_id": chat_id, "text": text, "parse_mode": parse_mode}
     async with aiohttp.ClientSession() as x:
       async with x.post(url, json=data) as z:
         return await z.json()

0b73870d478134e66a39c3d4fa7759b92808260b
Otazuki004 <otazuki004@gmail.com>
2025-02-26 18:38:14 +0530

    Update

--- Nexgram/types/chats/user.py
+++ Nexgram/types/chats/user.py
@@ -1,2 +1,20 @@
+import json
+
 class User:
-  pass
+  def __init__(
+    self,
+    id: int,
+    first_name: str,
+    last_name: str = None,
+    username: str = None,
+  ):
+    self.id = id
+    self.first_name = first_name
+    if last_name: self.last_name = last_name
+    if username: self.username = username
+  
+  def __str__(self):
+    return json.dumps(self.__dict__, indent=2)
+    
+  def __repr__(self):
+    return json.dumps(self.__dict__, indent=2)
--- Nexgram/types/message.py
+++ Nexgram/types/message.py
@@ -1,11 +1,18 @@
+import json
+
 class Message:
   def __init__(
     self,
     id: int,
-    from_user,
-    chat,
+    from_user: "Nexgram.types.User",
     text: str = None,
   ):
-    pass
+    self.id = id
+    self.from_user = from_user
+    self.text = text
+  
   def __str__(self):
-    return json.dumps(self.__dict__)
+    return json.dumps(self.__dict__, indent=2)
+  
+  def __repr__(self):
+    return json.dumps(self.__dict__, indent=2)

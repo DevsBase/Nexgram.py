fa27b2ad056bd5dac2dca8cf15a67ccbbcb5996d
Otazuki004 <otazuki004@gmail.com>
2025-02-27 16:25:35 +0530

    Update

--- Nexgram/methods/send_message.py
+++ Nexgram/methods/send_message.py
@@ -19,4 +19,4 @@ class sendMessage:
           error = z.get('description').split(':', 1)[1]
           if 'bad request' in error_type.lower():
             raise BadRequest(error)
-        return await z.json()
+        return z

1a46bf5ab77f2b6ab73ba84a7325607f5b72312f
Otazuki004 <otazuki004@gmail.com>
2025-02-27 16:17:32 +0530

    Update

--- Nexgram/Methods/send_message.py
+++ Nexgram/Methods/send_message.py
@@ -1,4 +1,5 @@
 import aiohttp
+from Nexgram.errors import BadRequest
 
 class sendMessage:
   async def send_message(self, chat_id, text, reply_to_message_id: int = None, parse_mode=None):
@@ -12,4 +13,10 @@ class sendMessage:
     if reply_to_message_id: data["reply_to_message_id"] = reply_to_message_id
     async with aiohttp.ClientSession() as x:
       async with x.post(url, json=data) as z:
+        z = z.json()
+        if not z.get('ok') and z.get('error_code'):
+          error_type = z.get('description')
+          error = z.get('description').split(':', 1)[1]
+          if 'bad request' in error_type.lower():
+            raise BadRequest(error)
         return await z.json()
--- Nexgram/errors/__init__.py
+++ Nexgram/errors/__init__.py
@@ -1,2 +1,3 @@
 from .PollingAlreadyStartedError import PollingAlreadyStartedError 
 from .InvalidObject import InvalidObject
+from .BadRequest import BadRequest

581aab8a67ae4a82282673f539b95ba09a121b4d
Otazuki004 <otazuki004@gmail.com>
2025-03-01 10:33:16 +0530

    Update

--- Nexgram/errors/BadRequest.py
+++ Nexgram/errors/BadRequest.py
@@ -1,5 +1,5 @@
 class BadRequest(Exception):
-  def __init__(self, err):
-    super().__init__(err)
+  def __init__(self, message="Bad Request"):
+    super().__init__(message)
   def __str__(self):
-    return f"Nexgram.errors.BadRequest: {self.args[0]}"
+    return f"{self.__class__.__name__}: {self.args[0]}"

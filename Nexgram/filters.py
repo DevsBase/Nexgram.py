5a21c0eb268195b7692219f2831be4842f4437c0
Otazuki004 <otazuki004@gmail.com>
2025-03-03 14:39:51 +0530

    Update

--- Nexgram/filters.py
+++ Nexgram/filters.py
@@ -13,21 +13,26 @@ class Filter:
     log.info("Call triggered!")
     if self.is_async:
       return await self.func(*args, **kwargs)
-    return self.func(*args, **kwargs)
+    return self.func(*args, **kwargs)  # No await here
 
   def __and__(self, other):
     async def combined(*args, **kwargs):
-      return (await self(*args, **kwargs)) and (await other(*args, **kwargs))
+      r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
+      r2 = await other(*args, **kwargs) if other.is_async else other(*args, **kwargs)
+      return r1 and r2
     return Filter(combined)
 
   def __or__(self, other):
     async def combined(*args, **kwargs):
-      return (await self(*args, **kwargs)) or (await other(*args, **kwargs))
+      r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
+      r2 = await other(*args, **kwargs) if other.is_async else other(*args, **kwargs)
+      return r1 or r2
     return Filter(combined)
 
   def __invert__(self):
     async def inverted(*args, **kwargs):
-      return not (await self(*args, **kwargs))
+      r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
+      return not r1
     return Filter(inverted)
     
 def create(func):

9cc0a6eb937dfa2ad6a797f793b8b90831112f7a
Otazuki004 <otazuki004@gmail.com>
2025-03-03 15:41:09 +0530

    Update

--- Nexgram/api/__init__.py
+++ Nexgram/api/__init__.py
@@ -2,16 +2,16 @@
 import aiohttp
 
 class Api:
-  async def post(self, url, json: dict = None, need_json=True):
+  async def post(self, url, json: dict = None, need_text=False):
     async with aiohttp.ClientSession() as mano:
       async with mano.post(url=url, json=json) as ily:
-        if need_json:
-          return await ily.json()
-        else: return ily
+        if need_text:
+          return await ily.text()
+        else: return await ily.json()
 
-  async def get(self, url, params=None, need_json=True):
+  async def get(self, url, params=None, need_text=False):
     async with aiohttp.ClientSession() as mano:
       async with mano.get(url=url, params=params) as ily:
-        if need_json:
-          return await ily.json()
-        else: return ily
+        if need_text:
+          return await ily.text()
+        else: return await ily.json()

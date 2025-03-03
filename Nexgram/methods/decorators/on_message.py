24205e6b04993612c7050298e2479882f6334c8e
Otazuki004 <otazuki004@gmail.com>
2025-03-03 14:12:49 +0530

    Update

--- Nexgram/methods/decorators/on_message.py
+++ Nexgram/methods/decorators/on_message.py
@@ -1,24 +1,29 @@
+import asyncio
+import inspect
 from Nexgram.filters import Filter
 from Nexgram import filters as f
-import inspect
-import asyncio
 
 class OnMessage:
+  def __init__(self):
+    self.on_message_listeners = {}  
+
   def on_message(self, filters):
     if not isinstance(filters, Filter):
-      filters = f.create(filters)
+      filters = f.create(filters)  
+
     def decorator(mano):
       if mano in self.on_message_listeners:
         raise Exception("You have already used this same decorator, you cannot use it multiple times!")
-      self.on_message_listeners[mano] = filters
-      return mano 
+      self.on_message_listeners[mano] = filters  
+      return mano
     return decorator
+
   async def trigger(self, *args):
     for func, filter_func in self.on_message_listeners.items():
       if inspect.iscoroutinefunction(filter_func.func):
-        passed = await filter_func(*args) 
+        passed = await filter_func(*args)  
       else:
-        passed = await asyncio.to_thread(filter_func, *args)  
+        passed = await asyncio.to_thread(filter_func.func, *args)  
 
       if passed:
         await func(*args)

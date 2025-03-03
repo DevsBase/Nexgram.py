bf7d32a2126fba410bea19ae74eb3bd72bad28b8
Otazuki004 <otazuki004@gmail.com>
2025-03-03 11:59:07 +0530

    Update

--- Nexgram/methods/decorators/on_message.py
+++ Nexgram/methods/decorators/on_message.py
@@ -1,5 +1,6 @@
 class OnMessage:
   def on_message(self, filters=None):
+    if filters == '': filters = None
     def decorator(mano):
       if mano in self.on_message_listeners:
         raise Exception("You have already used this same decorator, you cannot use it multipul times!")

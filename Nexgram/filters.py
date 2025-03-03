2208cb70dcc9ec50c843e59a73687dc19af0ae5c
Otazuki004 <otazuki004@gmail.com>
2025-03-03 14:58:19 +0530

    Update

--- Nexgram/filters.py
+++ Nexgram/filters.py
@@ -8,27 +8,22 @@ class Filter:
   def __init__(self, func):
     self.func = func
     self.is_async = inspect.iscoroutinefunction(func)
-
   async def __call__(self, *args, **kwargs):
-    log.info("Call triggered!")
     if self.is_async:
       return await self.func(*args, **kwargs)
-    return self.func(*args, **kwargs)  # No await here
-
+    return self.func(*args, **kwargs)
   def __and__(self, other):
     async def combined(*args, **kwargs):
       r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
       r2 = await other(*args, **kwargs) if other.is_async else other(*args, **kwargs)
       return r1 and r2
     return Filter(combined)
-
   def __or__(self, other):
     async def combined(*args, **kwargs):
       r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
       r2 = await other(*args, **kwargs) if other.is_async else other(*args, **kwargs)
       return r1 or r2
     return Filter(combined)
-
   def __invert__(self):
     async def inverted(*args, **kwargs):
       r1 = await self(*args, **kwargs) if self.is_async else self(*args, **kwargs)
@@ -40,5 +35,10 @@ def create(func):
     return func
   name = getattr(func, "__name__", "CustomFilter")
   return type(name, (Filter,), {"__call__": func})(func)
-    
+     
 text = create(lambda _, message: message.text)
+
+def command(cmd, prefix=['/']):
+  async def wrapper(_, __, m):
+    return any(m.text.startswith(p) and m.text[len(p):].startswith(cmd) for p in prefix)
+  return create(wrapper)

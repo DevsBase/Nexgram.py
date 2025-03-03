7ec4b71b36dedde8d05056a30141cbdee19418ed
Otazuki004 <otazuki004@gmail.com>
2025-03-03 20:13:55 +0530

    Update

--- Nexgram/Methods/__init__.py
+++ Nexgram/Methods/__init__.py
@@ -2,11 +2,13 @@ from .send_message import sendMessage
 from .dispatch_update import Dispatch
 from .decorators import Decorators
 from .call import Call
+from .forward_messages import ForwardMessages
 
 class Methods(
   sendMessage,
   Dispatch,
   Decorators,
   Call,
+  ForwardMessages,
 ):
   pass

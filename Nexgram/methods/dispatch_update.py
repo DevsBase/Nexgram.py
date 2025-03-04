5933468840dbd7efba74a2a32f8372179fdf2ffc
Otazuki004 <otazuki004@gmail.com>
2025-03-04 10:45:43 +0530

    Update

--- Nexgram/methods/dispatch_update.py
+++ Nexgram/methods/dispatch_update.py
@@ -10,36 +10,7 @@ class Dispatch:
     if update.get('message'):
       try:
         m = update.get('message')
-        frm = m.get('from')
-        ch = m.get('chat')
-        from_user = User(
-          client=self,
-          id=frm['id'],
-          first_name=frm['first_name'],
-          last_name=frm.get('last_name'),
-          username=frm.get('username'),
-          is_bot=frm.get('is_bot'),
-          is_self=frm['id'] == self.me.id,
-        )
-        chat = Chat(
-          id=ch['id'],
-          title=ch.get('title'),
-          first_name=ch.get('first_name'),
-          last_name=ch.get('last_name'),
-          type=ch.get('type'),
-          username=ch.get('username')
-        )
-        reply_to_message = None
-            
-        message = Message(
-          client=self,
-          id=m['message_id'],
-          from_user=from_user,
-          chat=chat,
-          reply_to_message=reply_to_message,
-          text=m.get('text')
-        )
-        
+        message = await self.create_message(m)
         for x in self.on_message_listeners:
           asyncio.create_task(self.call(self.on_message_listeners, x, self, message))
       except Exception as e:

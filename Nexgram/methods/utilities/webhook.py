e2efa247d067b671e690c76df01989d79e7be1a1
Otazuki004 <otazuki004@gmail.com>
2025-03-06 16:25:09 +0530

    Update

--- Nexgram/methods/utilities/webhook.py
+++ Nexgram/methods/utilities/webhook.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 
 class Webhook:
-  async def __takeCareWebhook(self, request):
+  async def _takeCareWebhook(self, request):
     data = request.json()
     self.log.info(f"Got webhook: {data}")
     return web.json_response({"status": "ok"})
@@ -10,7 +10,7 @@ class Webhook:
       raise ConnectionError("Client is not connected. please start the client.")
     self.webhook = True
     app = web.Application()
-    app.router.add_post("/webhook", self.__takeCareWebhook)
+    app.router.add_post("/", self.takeCareWebhook)
     runner = web.AppRunner(app)
     self.log.info("Success. now listening updates from webhook.")
     await runner.setup()

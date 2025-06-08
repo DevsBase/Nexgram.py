bf2ad36900b92ba426e0ef86215bb48a6d827ce1
Otazuki004 <otazuki004@gmail.com>
2025-06-08 17:10:37 +0000

    Update

--- Nexgram/methods/decorators/on_raw_update.py
+++ Nexgram/methods/decorators/on_raw_update.py
@@ -1,3 +1,6 @@
 class OnRawUpdate:
-  def on_raw_update(self, func):
-    self.on_raw_update_listeners[func] = True
+  def on_raw_update(self):
+    def decorator(func):
+      self.on_raw_update_listeners[func] = True
+      return func
+    return decorator

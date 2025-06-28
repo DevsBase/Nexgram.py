9428fe6c95924314195fa157163e43bdd0a7518c
Otazuki004 <otazuki004@gmail.com>
2025-06-28 16:17:24 +0000

    test

--- Nexgram/methods/decorators/on_disconnect.py
+++ Nexgram/methods/decorators/on_disconnect.py
@@ -1,8 +1,11 @@
 import asyncio
 
 class OnDisconnect:
-  def on_disconnect(self, mano):
-    self.on_disconnect_listeners[mano] = None
+  def on_disconnect(self):
+    def decorator(func):
+      self.on_disconnect_listeners[func] = {}
+      return decorator
+    return decorator
   async def trigger_disconnect(self):
     for mano in self.on_disconnect_listeners:
       asyncio.create_task(mano(self))
--- Nexgram/methods/decorators/on_raw_update.py
+++ Nexgram/methods/decorators/on_raw_update.py
@@ -1,6 +1,6 @@
 class OnRawUpdate:
   def on_raw_update(self):
     def decorator(func):
-      self.on_raw_update_listeners[func] = True
+      self.on_raw_update_listeners[func] = {}
       return func
     return decorator
